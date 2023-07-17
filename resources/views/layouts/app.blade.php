51090dcfd924c13b9ae84b5aee322156af2b83af
luizalvesot <alvesluizotavio23@gmail.com>
2023-07-17 10:17:54 -0300

    Ajuste no navbar e footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,64 +32,22 @@
 </head>
 <body style="background-color: rgb(247, 247, 247)">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: rgb(55, 61, 75);">
-            <div class="container">
-                <a class="navbar-brand text-white" href="{{ url('/') }}">
-                    <img src="{{ asset('img/easytec_logo_mini_2.png') }}" width="130" alt="Easytec Brasil" loading="Easytec Brasil" />
+        
+        <div class="container-fluid p-4 bg-white shadow-sm">
+            <div class="row">
+                <a class="" align="center" href="{{ url('/') }}">
+                    <img src="{{ asset('img/easytec-transparente-2.png') }}" width="250" alt="Easytec Brasil" loading="Easytec Brasil" />
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <!--li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li-->
-                            @endif
-
-                            @if (Route::has('register'))
-                                <!--li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li-->
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
             </div>
-        </nav>
+            <div class="row">
+                <h4 align="center">Cobertura de Atendimento - Easytec Brasil</h4>
+            </div> 
+        </div>
 
         <main class="py-4">
             @yield('content')
             <!-- Footer -->
-        <footer class="p-">
+        <footer class="p-4 mt-4 bg-white">
             <div>
                 <p class="text-center text-body fw-light">© {{ date('Y') }} Copyright Easytec Brasil.</p>
             </div>
